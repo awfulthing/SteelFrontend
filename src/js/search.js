f88ec5726c676531ec4d01eb136dc1caaa3119ff
awfulthing <awwfultthing@gmail.com>
2023-05-29 22:37:45 +0700

    оптимизировала шапку и футер

--- src/js/search.js
+++
@@ -1,6 +0,0 @@
-var searchIcon = document.getElementById("searchIcon");
-var searchBox = document.querySelector(".search_box");
-
-searchIcon.addEventListener("click", function () {
-    searchBox.style.display = searchBox.style.display === "none" ? "block" : "none";
-});
